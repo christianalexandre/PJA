d12868ab64457534b1da7bdda23d3f67d836a846
gabrielsebrao <gabrielsebrao06@gmail.com>
2025-01-20 17:12:56 -0300

    feat: create 'add task' view

--- tasks/gabrielsebrao/ToDo/app/src/main/java/com/example/todo/AddFragment.kt
+++ tasks/gabrielsebrao/ToDo/app/src/main/java/com/example/todo/AddFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.todo.databinding.FragmentAddBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,9 +21,11 @@ class AddFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var binding: FragmentAddBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
@@ -33,8 +36,11 @@ class AddFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        binding = FragmentAddBinding.inflate(layoutInflater)
+        binding?.inputLayoutAddTitle?.layoutInput?.hint = getString(R.string.title)
+        binding?.inputLayoutAddContent?.layoutInput?.hint = getString(R.string.content)
+        binding?.inputLayoutAddContent?.editText?.minHeight = 300
+        return binding?.root
     }
 
     companion object {
