30bf9117f333797dbd1de4e8c832508fff742e4c
gabrielsebrao <gabrielsebrao06@gmail.com>
2025-02-28 10:21:53 -0300

    feat: change image bottom sheet image

--- tasks/gabrielsebrao/ToDo/app/src/main/java/com/example/todo/utils/bottomsheet/PhotoAccessBottomSheetFragment.kt
+++ tasks/gabrielsebrao/ToDo/app/src/main/java/com/example/todo/utils/bottomsheet/PhotoAccessBottomSheetFragment.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.todo.databinding.BottomSheetLayoutBinding
+import com.example.todo.databinding.ItemBottomSheetLayoutBinding
 import com.example.todo.utils.listener.PhotoAccessListener
 
 class PhotoAccessBottomSheetFragment(
     private val listener: PhotoAccessListener
-): BaseBottomSheetFragment<BottomSheetLayoutBinding>() {
+): BaseBottomSheetFragment<ItemBottomSheetLayoutBinding>() {
 
-    override fun inflateBinding(inflater: LayoutInflater, container: ViewGroup?): BottomSheetLayoutBinding {
-        return BottomSheetLayoutBinding.inflate(inflater, container, false)
+    override fun inflateBinding(inflater: LayoutInflater, container: ViewGroup?): ItemBottomSheetLayoutBinding {
+        return ItemBottomSheetLayoutBinding.inflate(inflater, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
